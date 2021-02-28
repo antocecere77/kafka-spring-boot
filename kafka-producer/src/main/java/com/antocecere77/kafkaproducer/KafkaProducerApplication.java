56e294deafe6b646c9ecb7334329503c0a871721
antocecere77 <antocecere77@gmail.com>
2021-02-28 18:09:27 +0100

    producing message with key

--- kafka-producer/src/main/java/com/antocecere77/kafkaproducer/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/com/antocecere77/kafkaproducer/KafkaProducerApplication.java
@@ -1,6 +1,7 @@
 package com.antocecere77.kafkaproducer;
 
 import com.antocecere77.kafkaproducer.producer.HelloKakfaProducer;
+import com.antocecere77.kafkaproducer.producer.KafkaKeyProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,11 +12,22 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class KafkaProducerApplication implements CommandLineRunner {
 
+	@Autowired
+	private KafkaKeyProducer kafkaKeyProducer;
+
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaProducerApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		for (int i = 0; i < 10000; i++) {
+			var key = "key-" + (i%4);
+			var data = "data " + i + " with key " + key;
+			kafkaKeyProducer.send(key, data);
+
+			Thread.sleep(500);
+		}
 	}
 }

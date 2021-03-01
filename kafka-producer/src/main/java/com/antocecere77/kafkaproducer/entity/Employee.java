ad96532ed9239b6386e7df49389f1e01be6e8972
antocecere77 <antocecere77@gmail.com>
2021-03-01 06:46:18 +0100

    customize json format

--- kafka-producer/src/main/java/com/antocecere77/kafkaproducer/entity/Employee.java
+++ kafka-producer/src/main/java/com/antocecere77/kafkaproducer/entity/Employee.java
@@ -1,13 +1,20 @@
 package com.antocecere77.kafkaproducer.entity;
 
+import com.antocecere77.kafkaproducer.json.CustomLocalDateSerializer;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.time.LocalDate;
 
 public class Employee {
 
+    @JsonProperty("employee_id")
     private String employee;
 
     private String name;
 
+    @JsonProperty("birth_date")
+    @JsonSerialize(using = CustomLocalDateSerializer.class)
     private LocalDate birthDate;
 
     public Employee(String employee, String name, LocalDate birthDate) {

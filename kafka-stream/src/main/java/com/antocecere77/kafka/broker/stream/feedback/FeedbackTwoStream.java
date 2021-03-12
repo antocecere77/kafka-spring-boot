ada92b95365383b9913d3949aed5661df9094990
antocecere77 <antocecere77@gmail.com>
2021-03-12 07:04:24 +0100

    who owns this feedback?

--- kafka-stream/src/main/java/com/antocecere77/kafka/broker/stream/feedback/FeedbackTwoStream.java
+++ kafka-stream/src/main/java/com/antocecere77/kafka/broker/stream/feedback/FeedbackTwoStream.java
@@ -1,22 +1,22 @@
 package com.antocecere77.kafka.broker.stream.feedback;
 
-import com.antocecere77.kafka.broker.message.FeedbackMessage;
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.ValueMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.support.serializer.JsonSerde;
+import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.Set;
 
-import static java.util.stream.Collectors.toList;
+import com.antocecere77.kafka.broker.message.FeedbackMessage;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonSerde;
 
-//@Configuration
-public class FeedbackOneStream {
+@Configuration
+public class FeedbackTwoStream {
 
     private static final Set<String> GOOD_WORDS = Set.of("happy", "good", "helpful");
 
@@ -26,26 +26,15 @@ public class FeedbackOneStream {
         var feedbackSerde = new JsonSerde<>(FeedbackMessage.class);
 
         var goodFeedbackStream = builder.stream("t.commodity.feedback", Consumed.with(stringSerde, feedbackSerde))
-                .flatMapValues(mapperGoodWords());
-        goodFeedbackStream.to("t.commodity.feedback-one-good");
+                .flatMap((k, v) -> Arrays
+                        .asList(v.getFeedback().replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+")).stream()
+                        .filter(word -> GOOD_WORDS.contains(word)).distinct()
+                        .map(goodWord -> KeyValue.pair(v.getLocation(), goodWord)).collect(toList()));
+        goodFeedbackStream.to("t.commodity.feedback-two-good");
 
         return goodFeedbackStream;
     }
 
-    private ValueMapper<FeedbackMessage, Iterable<String>> mapperGoodWords() {
-        return feedbackMessage -> Arrays
-                .asList(feedbackMessage.getFeedback().replaceAll("[^a-zA-Z ]", "")
-                        .toLowerCase()
-                        .split("\\s+"))
-                .stream()
-                .filter(word -> GOOD_WORDS.contains(word))
-                .distinct()
-                .collect(toList());
-    }
 }
-
-
-
-
 
 
